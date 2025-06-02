693b87fbd04afb4fa518b8bc5fd7fc77433c7491
AathifahDC <aathifah800@gmail.com>
2025-06-02 10:00:28 +0700

    recent updates

--- xamppfiles/htdocs/webproject/js/script.js
+++
@@ -1,8 +0,0 @@
-function validateLogin() {
-  const username = document.querySelector('input[name="username"]').value;
-  if (username.length < 3) {
-    alert("Username must be at least 3 characters long");
-    return false;
-  }
-  return true;
-}
